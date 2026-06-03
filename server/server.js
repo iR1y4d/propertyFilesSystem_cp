ecb60ab1157a101ec4c0fb544ec21656a9f452fe
Riyad <Riyad.hu95@gmail.com>
2026-06-03 14:09:49 +0200

    add backend test again

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-});
 app.get("/", (req, res) => {
   res.send("✅ Backend is working");
 });
+
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+});
+
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
