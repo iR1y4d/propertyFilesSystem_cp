11cdac70e32304531351d1a37a554bca957e5c08
Riyad <Riyad.hu95@gmail.com>
2026-06-03 13:32:23 +0200

    fix railway binding issue

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
 
